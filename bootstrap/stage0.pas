fc3657b4d9a7a79d8b630a1c5869f2e89d61098a
Jacobo Tarrio <jtarrio@gmail.com>
2022-12-25 12:03:18 -0500

    Simpler lexer

--- bootstrap/stage0.pas
+++ bootstrap/stage0.pas
@@ -1,296 +1,197 @@
 program stage0(Input, Output, StdErr);
 
 type 
-  TokenId = (TkUnknown, TkEof, TkComment, TkIdentifier, TkNumber, TkString,
-             TkPlus, TkMinus, TkAsterisk, TkSlash, TkEquals, TkLessthan,
-             TkMorethan, TkLbracket, TkRbracket, TkDot, TkComma, TkColon,
-             TkSemicolon, TkCaret, TkLparen, TkRparen, TkNotEquals,
-             TkLessOrEquals, TkMoreOrEquals, TkAssign, TkRange, TkAnd, TkArray,
-             TkBegin, TkCase, TkConst, TkDiv, TkDo, TkDownto, TkElse, TkEnd,
-             TkFile, TkFor, TkFunction, TkGoto, TkIf, TkIn, TkLabel, TkMod,
-             TkNil, TkNot, TkOf, TkOr, TkPacked, TkProcedure, TkProgram,
-             TkRecord, TkRepeat, TkSet, TkThen, TkTo, TkType, TkUntil, TkVar,
-             TkWhile, TkWith);
-  Token = record
-    Id : TokenId;
-    Value: string
+  TLxTokenId = (TkUnknown, TkEof, TkComment, TkIdentifier, TkNumber, TkString,
+                TkPlus, TkMinus, TkAsterisk, TkSlash, TkEquals, TkLessthan,
+                TkMorethan, TkLbracket, TkRbracket, TkDot, TkComma, TkColon,
+                TkSemicolon, TkCaret, TkLparen, TkRparen, TkNotEquals,
+                TkLessOrEquals, TkMoreOrEquals, TkAssign, TkRange, TkAnd,
+                TkArray, TkBegin, TkCase, TkConst, TkDiv, TkDo, TkDownto,
+                TkElse, TkEnd, TkFile, TkFor, TkFunction, TkGoto, TkIf, TkIn,
+                TkLabel, TkMod, TkNil, TkNot, TkOf, TkOr, TkPacked, TkProcedure,
+                TkProgram, TkRecord, TkRepeat, TkSet, TkThen, TkTo, TkType,
+                TkUntil, TkVar, TkWhile, TkWith);
+  TLxToken = record
+    Id : TLxTokenId;
+    Value : string
   end;
-const 
-  Symbols: array[1..16] of string = ('+', '-', '*', '/', '=', '<', '>', '[',
-                                     ']', '.', ',', ':', ';', '^', '(', ')');
-  Words: array[1..35] of string = ('AND', 'ARRAY', 'BEGIN', 'CASE', 'CONST',
-                                   'DIV', 'DO', 'DOWNTO', 'ELSE', 'END', 'FILE',
-                                   'FOR', 'FUNCTION', 'GOTO', 'IF', 'IN',
-                                   'LABEL', 'MOD', 'NIL', 'NOT', 'OF', 'OR',
-                                   'PACKED', 'PROCEDURE', 'PROGRAM', 'RECORD',
-                                   'REPEAT', 'SET', 'THEN', 'TO', 'TYPE',
-                                   'UNTIL', 'VAR', 'WHILE', 'WITH');
 var 
-  CurrentLine: string;
-  ThisToken: Token;
+  LxLine : string;
+  LxToken : TLxToken;
 
-function IsAlpha(Chr : char) : boolean;
+function LxIsAlpha(Chr : char) : boolean;
 begin
-  IsAlpha := ((Chr >= 'a') and (Chr <= 'z')) or ((Chr >= 'A') and (Chr <= 'Z'))
+  LxIsAlpha := ((Chr >= 'a') and (Chr <= 'z'))
+               or ((Chr >= 'A') and (Chr <= 'Z'))
 end;
 
-function IsDigit(Chr : char) : boolean;
+function LxIsDigit(Chr : char) : boolean;
 begin
-  IsDigit := (Chr >= '0') and (Chr <= '9')
+  LxIsDigit := (Chr >= '0') and (Chr <= '9')
 end;
 
-function IsAlphaNum(Chr : char) : boolean;
+function LxIsAlphaNum(Chr : char) : boolean;
 begin
-  IsAlphaNum := IsAlpha(Chr) or IsDigit(Chr)
+  LxIsAlphaNum := LxIsAlpha(Chr) or LxIsDigit(Chr)
 end;
 
-function IsTokenWaiting : boolean;
-var 
-  Comment : (No, Braces, Parens);
+function LxIsTokenWaiting : boolean;
 begin
-  Comment := No;
   repeat
-    while (Length(CurrentLine) = 0) and not Eof(Input) do
-    begin
-      readln(Input, CurrentLine);
-    end;
-    while (Length(CurrentLine) > 0) and (CurrentLine[1] = ' ') do
-      delete(CurrentLine, 1, 1);
-    if Comment = No then
-    begin
-      if CurrentLine[1] = '{' then
-      begin
-        Comment := Braces;
-        delete(CurrentLine, 1, 1)
-      end
-      else if (Length(CurrentLine) > 1)
-              and (CurrentLine[1] = '(')
-              and (CurrentLine[2] = '*') then
-      begin
-        Comment := Parens;
-        delete(CurrentLine, 1, 2)
-      end
-    end;
-    while (Comment = Braces) and (Length(CurrentLine) > 0) do
-    begin
-      if CurrentLine[1] = '}' then
-        Comment := No;
-      delete(CurrentLine, 1, 1)
-    end;
-    while (Comment = Parens) and (Length(CurrentLine) > 0) do
-    begin
-      if (Length(CurrentLine) > 1) and (CurrentLine[1] = '*') and (CurrentLine[2
-         ] = ')') then
-      begin
-        Comment := No;
-        delete(CurrentLine, 1, 2)
-      end
-      else
-        delete(CurrentLine, 1, 1)
-    end;
-  until (Comment = No) and (Length(CurrentLine) > 0) or Eof(Input);
-  IsTokenWaiting := Length(CurrentLine) > 0
+    while (Length(LxLine) = 0) and not Eof(Input) do
+      readln(Input, LxLine);
+    while (Length(LxLine) > 0) and (LxLine[1] = ' ') do
+      delete(LxLine, 1, 1);
+  until Eof(Input) or (Length(LxLine) > 0);
+  LxIsTokenWaiting := Length(LxLine) > 0
 end;
 
-procedure GetSymbol(var Token : Token; Id: TokenId; Length: integer);
+procedure LxGetSymbol(Id : TLxTokenId; Length : integer);
 begin
-  Token.Value := copy(CurrentLine, 1, length);
-  delete(CurrentLine, 1, length);
-  Token.Id := Id;
+  LxToken.Id := Id;
+  LxToken.Value := copy(LxLine, 1, length);
+  delete(LxLine, 1, length);
 end;
 
-procedure GetIdentifier(var Token: Token);
+procedure LxGetIdentifier;
+const 
+  Words : array[1..35] of string = ('AND', 'ARRAY', 'BEGIN', 'CASE', 'CONST',
+                                    'DIV', 'DO', 'DOWNTO', 'ELSE', 'END',
+                                    'FILE', 'FOR', 'FUNCTION', 'GOTO', 'IF',
+                                    'IN', 'LABEL', 'MOD', 'NIL', 'NOT', 'OF',
+                                    'OR', 'PACKED', 'PROCEDURE', 'PROGRAM',
+                                    'RECORD', 'REPEAT', 'SET', 'THEN', 'TO',
+                                    'TYPE', 'UNTIL', 'VAR', 'WHILE', 'WITH');
 var 
-  Chr: char;
-  Pos: integer;
-  InToken: boolean;
-  WordId: TokenId;
+  Chr : char;
+  Pos : integer;
+  InToken : boolean;
+  WordId : TLxTokenId;
 begin
   Pos := 0;
-  InToken := True;
-  WordId := TkAnd;
-  while (Pos < Length(CurrentLine)) and InToken do
+  InToken := true;
+  while (Pos < Length(LxLine)) and InToken do
   begin
-    Chr := CurrentLine[Pos + 1];
-    InToken := IsAlphaNum(Chr);
+    Chr := LxLine[Pos + 1];
+    InToken := LxIsAlphaNum(Chr);
     if InToken then Pos := Pos + 1
   end;
-  GetSymbol(Token, TkIdentifier, Pos);
-  for Pos := 1 to Length(Token.Value) do
-    Token.Value[Pos] := UpCase(Token.Value[Pos]);
+  LxGetSymbol(TkIdentifier, Pos);
+  for Pos := 1 to Length(LxToken.Value) do
+    LxToken.Value[Pos] := UpCase(LxToken.Value[Pos]);
+  WordId := TkAnd;
   for Pos := 1 to 35 do
     begin
-      if Token.Value = Words[Pos] then
-        Token.Id := WordId;
+      if LxToken.Value = Words[Pos] then
+        LxToken.Id := WordId;
       WordId := succ(WordId)
     end
 end;
 
-procedure GetNumber(var Token : Token);
+procedure LxGetNumber;
 var 
-  Chr: char;
+  Chr : char;
   Pos : integer;
-  Part : (None, Sign, Int, Dot, Frac, E, Mag);
+  InToken : boolean;
 begin
   Pos := 0;
-  Part := Sign;
-  while (Pos < Length(CurrentLine)) and (Part <> None) do
+  InToken := true;
+  while (Pos < Length(LxLine)) and InToken do
   begin
-    Chr := CurrentLine[Pos + 1];
-    if (Chr = '-') or (Chr = '+') then
-    begin
-      if Part = Sign then
-        Part := Int
-      else if Part = E then
-             Part := Mag
-      else
-        Part := None
-    end
-    else if Chr = 'e' then
-    begin
-      if (Part = Int) or (Part = Frac) then
-        Part := E
-      else
-        Part := None
-    end
-    else if Chr = '.' then
-    begin
-      if Part = Int then
-        Part := Dot
-      else
-        Part := None
-    end
-    else if IsDigit(Chr) then
-    begin
-      if Part = Sign then
-        Part := Int
-      else if Part = Dot then
-             Part := Frac
-      else if Part = E then
-             Part := Mag
-    end
-    else
-      Part := None;
-    if Part <> None then
-      Pos := Pos + 1;
+    Chr := LxLine[Pos + 1];
+    InToken := LxIsDigit(Chr);
+    if InToken then Pos := Pos + 1
   end;
-  if (CurrentLine[Pos] = '.') or (CurrentLine[Pos] = 'e') then
-    Pos := Pos - 1;
-  GetSymbol(Token, TkNumber, Pos);
+  LxGetSymbol(TkNumber, Pos);
 end;
 
-procedure GetString(var Token : Token);
+procedure LxGetString;
 var 
-  Chr: char;
-  Pos: integer;
-  Instring: boolean;
+  Chr : char;
+  Pos : integer;
+  Instring : boolean;
 begin
   Pos := 1;
   Instring := True;
   while Instring do
   begin
     Pos := Pos + 1;
-    Chr := CurrentLine[Pos];
+    Chr := LxLine[Pos];
     if Chr = '''' then
     begin
-      if (Pos + 1 < Length(CurrentLine)) and (CurrentLine[Pos + 1] = '''')
-        then
-      begin
-        Pos := Pos + 2
-      end
+      if (Length(LxLine) > Pos + 1) and (LxLine[Pos + 1] = '''') then
+        Pos := Pos + 1
       else
         Instring := False;
     end;
   end;
-  GetSymbol(Token, TkString, Pos);
+  LxGetSymbol(TkString, Pos);
 end;
 
-procedure ReadToken(var Token : Token);
+procedure LxGetComment;
 var 
-  Chr: char;
-  Nxt: char;
+  Comment : (No, Brace, Paren);
+  Saved : boolean;
+begin
+end;
+
+
+procedure LxReadToken;
+const 
+  Symbols : array[1..16] of string = ('+', '-', '*', '/', '=', '<', '>', '[',
+                                      ']', '.', ',', ':', ';', '^', '(', ')');
+var 
+  Chr : char;
+  Nxt : char;
   i : integer;
-  SymbolId : TokenId;
-
+  SymbolId : TLxTokenId;
 begin
-  Token.Value := '';
-  Token.Id := TkUnknown;
+  LxToken.Value := '';
+  LxToken.Id := TkUnknown;
 
-  repeat
-    if not IsTokenWaiting() then
-      Token.Id := TkEof
-    else
+  if not LxIsTokenWaiting() then
+    LxToken.Id := TkEof
+  else
+  begin
+    Chr := LxLine[1];
+
+    if LxIsAlpha(Chr) then
+      LxGetIdentifier();
+    if (LxToken.Id = TkUnknown) and LxIsDigit(Chr) then
+      LxGetNumber();
+    if (LxToken.Id = TkUnknown) and (Chr = '''') then
+      LxGetString();
+    if (LxToken.Id = TkUnknown) and (Chr = '{') then
+      LxGetComment();
+    if (LxToken.Id = TkUnknown) and (Length(LxLine) > 1) then
     begin
-      Chr := CurrentLine[1];
-
-      if IsAlpha(Chr) then
-        GetIdentifier(Token);
-      if (Token.Id = TkUnknown) and IsDigit(Chr) then
-        GetNumber(Token);
-      if (Token.Id = TkUnknown) and (Chr = '''') then
-        GetString(Token);
-      if (Token.Id = TkUnknown) and (Length(CurrentLine) > 1) then
-      begin
-        Nxt := CurrentLine[2];
-        if (Token.Id = TkUnknown) and ((Chr = '+') or (Chr = '-'))
-           and IsDigit(Nxt) then
-          GetNumber(Token);
-        if (Token.Id = TkUnknown) and (Chr = '<') and (Nxt = '>') then
-          GetSymbol(Token, TkNotEquals, 2);
-        if (Token.Id = TkUnknown) and (Chr = '<') and (Nxt = '=') then
-          GetSymbol(Token, TkLessOrEquals, 2);
-        if (Token.Id = TkUnknown) and (Chr = '>') and (Nxt = '=') then
-          GetSymbol(Token, TkMoreOrEquals, 2);
-        if (Token.Id = TkUnknown) and (Chr = ':') and (Nxt = '=') then
-          GetSymbol(Token, TkAssign, 2);
-        if (Token.Id = TkUnknown) and (Chr = '.') and (Nxt = '.') then
-          GetSymbol(Token, TkRange, 2);
-      end;
-      SymbolId := TkPlus;
-      for i := 1 to 16 do
-        begin
-          if (Token.Id = TkUnknown) and (Chr = Symbols[i]) then
-            GetSymbol(Token, SymbolId, 1);
-          SymbolId := succ(SymbolId)
-        end;
+      Nxt := LxLine[2];
+      if (LxToken.Id = TkUnknown) and (Chr = '(') and (Nxt = '*') then
+        LxGetComment();
+      if (LxToken.Id = TkUnknown) and (Chr = '<') and (Nxt = '>') then
+        LxGetSymbol(TkNotEquals, 2);
+      if (LxToken.Id = TkUnknown) and (Chr = '<') and (Nxt = '=') then
+        LxGetSymbol(TkLessOrEquals, 2);
+      if (LxToken.Id = TkUnknown) and (Chr = '>') and (Nxt = '=') then
+        LxGetSymbol(TkMoreOrEquals, 2);
+      if (LxToken.Id = TkUnknown) and (Chr = ':') and (Nxt = '=') then
+        LxGetSymbol(TkAssign, 2);
+      if (LxToken.Id = TkUnknown) and (Chr = '.') and (Nxt = '.') then
+        LxGetSymbol(TkRange, 2);
     end;
-  until Token.Id <> TkComment;
-end;
-
-procedure GetNextToken;
-begin
-  ReadToken(ThisToken);
-end;
-
-procedure ExpectToken(Id : TokenId);
-begin
-  if ThisToken.Id <> Id then
-  begin
-    writeln(StdErr, 'Expected token: ', Id, ', found ', ThisToken.Id,
-            ' (', ThisToken.Value, ')');
-    halt(1)
-  end
-end;
-
-procedure ExpectToken2(Id1, Id2 : TokenId);
-begin
-  if (ThisToken.Id <> Id1) and (ThisToken.Id <> Id2) then
-  begin
-    writeln(StdErr, 'Expected tokens: ', Id1, ' or ', Id2,
-            ', found ', ThisToken.Id, ' (', ThisToken.Value, ')');
-    halt(1)
-  end
-end;
-
-procedure ExpectAndGetNextToken(Id : TokenId);
-begin
-  ExpectToken(Id);
-  GetNextToken()
+    SymbolId := TkPlus;
+    for i := 1 to 16 do
+      begin
+        if (LxToken.Id = TkUnknown) and (Chr = Symbols[i]) then
+          LxGetSymbol(SymbolId, 1);
+        SymbolId := succ(SymbolId)
+      end;
+  end;
 end;
 
 begin
   repeat
-    GetNextToken();
-    writeln(Output, ThisToken.Id, ' [', ThisToken.Value, ']')
-  until (ThisToken.Id = TkEof) or (ThisToken.Id = TkUnknown)
+    LxReadToken();
+    writeln(Output, LxToken.Id, ' [', LxToken.Value, ']')
+  until (LxToken.Id = TkEof) or (LxToken.Id = TkUnknown)
 end.
