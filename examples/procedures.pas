1ec41a8461f04312c412659cd21088f16e50368a
Jacobo Tarrio <jtarrio@gmail.com>
2022-12-31 17:03:20 -0500

    Add functions to procedures example

--- examples/procedures.pas
+++ examples/procedures.pas
@@ -12,9 +12,23 @@ begin
   Num := Num + 1
 end;
 
+function Sum(N1 : integer; N2 : integer) : integer;
+begin
+  Sum := N1 + N2
+end;
+
+function SumInPlace(var N1 : integer; N2 : integer) : integer;
+begin
+  N1 := N1 + N2;
+  SumInPlace := N1
+end;
+
 begin
   Num := 1;
   Display(Num);
   Sum1(Num);
-  Display(Num)
+  Display(Num);
+  Display(Sum(Num, 1));
+  Display(SumInPlace(Num, 2));
+  Display(Num);
 end.
