0ebe5742f370508c82ad14847ddb8c6093a2a199
Jacobo Tarrio <jtarrio@gmail.com>
2023-03-23 17:12:09 -0400

    Remove compatibility shims from file.h

--- runtime/file.h
+++ runtime/file.h
@@ -53,28 +53,4 @@ void Write(PFile* file, PBoolean die_on_error, ...);
 
 void InitFile();
 
-#define CHDIR(dir) Chdir(dir, 0)
-#define MKDIR(dir) Mkdir(dir, 0)
-#define RMDIR(dir) Rmdir(dir, 0)
-#define GETDIR(drive, dir) Getdir(drive, dir, 0)
-
-#define ASSIGN(file, name) Assign(file, name, 0)
-#define CLOSE(file) Close(file, 0)
-#define RESET(file) Reset(file, 0, 0)
-#define REWRITE(file) Rewrite(file, 0, 0)
-#define EOF(file) Eof(file, 0)
-#define EOLN(file) Eoln(file, 0)
-#define SEEKEOF(file) Seekeof(file, 0)
-#define SEEKEOLN(file) Seekeoln(file, 0)
-#define FILEPOS(file) Filepos(file, 0)
-#define FILESIZE(file) Filesize(file, 0)
-#define SEEK(file, pos) Seek(file, pos, 0)
-#define FLUSH(file) Flush(file, 0)
-
-#define ERASE(file) Erase(file, 0)
-#define RENAME(file, name) Rename(file, name, 0)
-
-#define READ(file, ...) Read(file, 0, __VA_ARGS__)
-#define WRITE(file, ...) Write(file, 0, __VA_ARGS__)
-
 #endif  // __PASCUAL_FILE_H
